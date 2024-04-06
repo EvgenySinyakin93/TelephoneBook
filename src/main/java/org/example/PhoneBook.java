dedbe66016e2edbfe977053a92f43f80a0cc763a
EvgenySinyakin93 <evgeny.siniakin83@gmail.com>
2024-04-06 13:16:22 +0300

    Initial commit

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,16 @@
 package org.example;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class PhoneBook {
+
+    public static int add(String name, String phone) {
+        Map<String, String> phoneBook = new TreeMap<>();
+
+        if(!phoneBook.containsKey(name) && !phoneBook.containsValue(phone)){
+            phoneBook.putIfAbsent(name, phone);
+        }
+        return phoneBook.size();
+    }
 }
--- src/test/java/org/example/PhoneBookTest.java
+++ src/test/java/org/example/PhoneBookTest.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PhoneBookTest {
-
+    PhoneBook phoneBook = new PhoneBook();
+    @Test
+    void add() {
+        phoneBook.add("Oleg", "79105304792");
+        phoneBook.add("Elena", "79178308923");
+        int actual = phoneBook.add("Yura", "79564372901");
+        Assertions.assertEquals(3,actual);
+    }
 }
